ec4e5f7cec6d723c5707764243c50be5a1afb7d5
Andy Chan <taichan@amazon.com>
2021-09-03 00:41:42 -0600

    remove speak from noop

--- code/formatters.py
+++ code/formatters.py
@@ -296,7 +296,6 @@ def unformat_text(text: str) -> str:
 ctx.lists["self.formatters"] = formatters_words.keys()
 ctx.lists["self.prose_formatter"] = {
     "say": "NOOP",
-    "speak": "NOOP",
     "sentence": "CAPITALIZE_FIRST_WORD",
 }
 
